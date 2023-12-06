70679abef4fe224a731bbce3aa74bc9785e6a244
Oscar <oscarwruedakollmer@gmail.com>
2023-12-06 20:39:05 +0100

    PSP: P1) creación de la clase firebaseadmin y creacion de un dataholder que llama a esta clase

---
+++ lib/Singletone/DataHolder.dart
@@ -0,0 +1,17 @@
+import 'FirebaseAdmin.dart';
+
+class DataHolder {
+
+  static final DataHolder _dataHolder = DataHolder._internal();
+
+  FirebaseAdmin fbadmin=FirebaseAdmin();
+
+  void initDataHolder() {}
+
+  DataHolder._internal() {}
+  
+  factory DataHolder(){
+    return _dataHolder;
+  }
+
+}
